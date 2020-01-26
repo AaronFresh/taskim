b4b73180a177df3762af825d070493ece6ab2011
AaronFresh <aaronfresh08@gmail.com>
2020-01-26 12:35:05 +0200

    preparation du project

---
+++ AppClient/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+  //,styleUrls: ['./app.component.=scss']
+})
+export class AppComponent {
+  title = 'AppClient';
+
+ 
+}

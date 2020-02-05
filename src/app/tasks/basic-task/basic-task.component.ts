975904f402da94c1921d94a34e016c2ff6d3fcfc
AaronFresh <aaronfresh08@gmail.com>
2020-02-05 10:23:04 +0200

    remove = scss

--- src/app/tasks/basic-task/basic-task.component.ts
+++ src/app/tasks/basic-task/basic-task.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-basic-task',
-  templateUrl: './basic-task.component.html'
-  //,styleUrls: ['./basic-task.component.=scss']
+  templateUrl: './basic-task.component.html',
+  styleUrls: ['./basic-task.component.scss']
 })
 export class BasicTaskComponent implements OnInit {
 
--- src/app/tasks/dashboard/dashboard.component.ts
+++ src/app/tasks/dashboard/dashboard.component.ts
@@ -4,8 +4,8 @@ import { DataService } from '../../service/data.service';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
-  //,styleUrls: ['./dashboard.component.=scss']
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
